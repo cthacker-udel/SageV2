98d8a2cfa6f5269863191019c6f8afab5ff44eb6
Josh Lyon <joshlyon@udel.edu>
2021-09-21 14:09:45 -0400

    I think buttons for polling are working

--- src/commands/fun/poll.ts
+++ src/commands/fun/poll.ts
@@ -1,5 +1,5 @@
 import { BOT } from '@root/config';
-import { Message, MessageEmbed, MessageReaction } from 'discord.js';
+import { InteractionCollector, Message, MessageActionRow, MessageButton, MessageEmbed } from 'discord.js';
 import parse from 'parse-duration';
 import prettyMilliseconds from 'pretty-ms';
 import { Command } from '@lib/types/Command';
@@ -8,13 +8,27 @@ export default class extends Command {
 
 	description = `Have ${BOT.NAME} create a poll for you`;
 	aliases = ['vote'];
-	usage = '<timespan>|<question>|<choices...>';
+	usage = '<timespan>|<question>|<choice 1>|<choice2>|...|<choiceX>';
 	extendedHelp = 'You can have two to ten choices.';
 	runInDM = false;
 
 
-	async run(msg: Message, [timespan, question, ...choices]: [number, string, ...Array<string>]): Promise<Message> {
+	async run(msg: Message, [timespan, question, ...choices]: [number, string, ...Array<string>]): Promise<void> {
 		const emotes = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣', '🔟'].slice(0, choices.length);
+
+		const buttons: MessageButton[] = [];
+
+		let count = 0;
+		choices.forEach(value => {
+			buttons.push(new MessageButton()
+				.setCustomId(value)
+				.setStyle('PRIMARY')
+				.setLabel(value)
+				.setEmoji(emotes[count++]));
+		});
+
+		const rows = [new MessageActionRow().addComponents(buttons.slice(0, 5))];
+		if (buttons.length > 5) rows.push(new MessageActionRow().addComponents(buttons.slice(5)));
 
 		let choiceText = '';
 		choices.forEach((choice, option) => {
@@ -28,56 +42,85 @@ export default class extends Command {
 			.setFooter(`This poll ends in ${prettyMilliseconds(timespan, { verbose: true })}`)
 			.setColor('RANDOM');
 
+		const pollMsg = await msg.channel.send({ embeds: [pollEmbed], components: rows });
 
-		const pollMsg = await msg.channel.send({ embeds: [pollEmbed] });
 
-		emotes.forEach(emote => pollMsg.react(emote));
-
-		return pollMsg.awaitReactions({
-			filter: (reaction: MessageReaction) => emotes.includes(reaction.emoji.name),
+		const collector = new InteractionCollector(msg.client, {
+			filter: interaction => interaction.isButton(),
+			interactionType: 'MESSAGE_COMPONENT',
 			time: timespan
-		}).then(reactions => {
-			let maxVotes = 0;
-			reactions.forEach(reaction => {
-				if (reaction.users.cache.size > maxVotes) maxVotes = reaction.users.cache.size;
-			});
+		});
 
-			pollEmbed.fields = [{
-				name: question,
-				value: choiceText
-					.split('\n')
-					.map((choice, idx) => {
-						const votes = reactions.get(emotes[idx]).count - 1;
-						return `${choice} - ${votes} vote${votes === 1 ? '' : 's'}`;
-					}).join('\n'),
-				inline: false
-			}];
+		const voted: string[] = [];
+		collector.on('collect', interaction => {
+			if (!interaction.isButton()) return;
+			const voter = interaction.user;
 
-			if (maxVotes <= 1) {
-				pollMsg.edit({ embeds: [pollEmbed
-					.addField('Results', 'The poll ended but it looks like no one voted ☹!')
-					.setFooter('This poll has ended')] });
-
-				const embed = new MessageEmbed()
-					.setTitle(`Poll from ${msg.member.displayName}`)
-					.setDescription(`The poll ended but it looks like no one voted ☹!\n\n[Click to view poll](${pollMsg.url})`)
-					.setColor(pollEmbed.color);
-				return pollMsg.channel.send({ embeds: [embed] });
+			if (voted.includes(voter.id)) {
+				interaction.reply({
+					ephemeral: true,
+					content: 'Looks like you\'ve already voted in this poll. You cannot vote more than once!'
+				});
+				return;
 			}
 
-			const winners = reactions
-				.filter(reaction => reaction.users.cache.size >= maxVotes)
-				.map(reaction => choices[emotes.indexOf(reaction.emoji.name)]);
+			voted.push(voter.id);
 
-			pollMsg.edit({ embeds: [pollEmbed
-				.addField('Results', this.winMessage(winners, maxVotes - 1))
-				.setFooter('This poll has ended')] });
-			pollMsg.reactions.removeAll();
-			const embed = new MessageEmbed()
-				.setTitle(`Poll from ${msg.member.displayName} Result`)
-				.addField(question, `${this.winMessage(winners, maxVotes - 1)}\n\n[Click to view poll](${pollMsg.url})`)
-				.setColor(pollEmbed.color);
-			return pollMsg.channel.send({ embeds: [embed] });
+			interaction.reply({
+				ephemeral: true,
+				content: `Your vote for **${interaction.customId}** has been counted. Thanks for your participation!`
+			});
+		});
+
+		const winEmbed = new MessageEmbed()
+			.setTitle(`Poll from ${msg.member.displayName}`)
+			.setColor(pollEmbed.color);
+
+		collector.on('end', (collection) => {
+			const voters: string[] = [];
+			let winners: string[];
+			let winningCount = 0;
+			const votes: Map<string, number> = new Map<string, number>();
+			collection.forEach((value) => {
+				if (voters.includes(value.user.id) || !value.isButton()) return;
+				voters.push(value.user.id);
+				const key = value.customId;
+
+				// count votes up
+				if (!votes.has(key)) {
+					votes.set(key, 1);
+				} else {
+					votes.set(key, votes.get(key) + 1);
+				}
+
+				// set the winner(s)
+				if (votes.get(key) > winningCount) {
+					winningCount = votes.get(key);
+					winners = [key];
+				} else if (votes.get(key) === winningCount) {
+					winners.push(key);
+				}
+			});
+
+			pollMsg.components.forEach(row => row.components.forEach(comp => comp.setDisabled(true)));
+			if (winningCount === 0) {
+				pollMsg.edit({
+					embeds: [pollEmbed
+						.addField('Results', 'The poll ended but it looks like no one voted! ☹')
+						.setFooter('This poll has ended')],
+					components: pollMsg.components
+				});
+				winEmbed.setDescription(`The poll ended but it looks like no one voted! ☹\n\n[Click to view poll](${pollMsg.url})`);
+			} else {
+				pollMsg.edit({
+					embeds: [pollEmbed
+						.addField('Results', `${this.winMessage(winners, winningCount)}`)
+						.setFooter('This poll has ended')],
+					components: pollMsg.components
+				});
+				winEmbed.setDescription(`${this.winMessage(winners, winningCount)}\n\n[Click to view poll](${pollMsg.url})`);
+			}
+			msg.channel.send({ embeds: [winEmbed] });
 		});
 	}
 
